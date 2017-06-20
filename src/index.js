d582d18aa2a607e795bc8f0ce141f5a37a466c9c
Sol <sol@Sols-MacBook-Pro.local>
2017-06-20 16:46:31 -0400

    Scratch card switching done, new image assets for scratch card. MVP func complete some styling complete

--- src/index.js
+++ src/index.js
@@ -16,19 +16,19 @@ $(document).ready(function(){
     parent.postMessage('close-message', "*")
   })
 
-
   //Chevron animation and collapse of iframe
   $('.open-close-chevron').on('click', function() {
     $(this).toggleClass('open-close-chevron-flipped');
     $('.wrapper').toggleClass('hide-game');
   });
+
   //Picks Resources to append onto page
   if (config.game == "case") {
     var casesGame = require('cases.js');
     casesGame(coupon);
   } else if (config.game == "scratch") {
     var scratchGame = require('scratch.js');
-    scratchGame(coupon);
+    //scratchGame(coupon);
   } else {
     console.log('Could not find proper config variables.');
   }
